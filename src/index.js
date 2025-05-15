90095ee9b0011698a3b1896d7e507069534b6f36
Xinshuang Jin <jinxinshuang@gmail.com>
2025-05-15 11:02:09 -0400

    added semicolons and console.log

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 // No semicolons
-let red
-let blue = 20
-let yellow
+// let red
+// let blue = 20
+// let yellow
 
-red = blue + 2
-(yellow) = 42
+// red = blue + 2
+// (yellow) = 42
 
 
 // With semicolons
-// let red;
-// let blue = 20;
-// let yellow;
+let red;
+let blue = 20;
+let yellow;
 
-// red = blue + 2;
-// (yellow) = 42;
+red = blue + 2;
+(yellow) = 42;
+
+console.log(red) 
+console.log(yellow)
+
+
+// 22
+// 42
